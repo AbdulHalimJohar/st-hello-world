ac41f25b094787f496bbe3d6b060a1fb9381db85
AbdulHalimJohar <gomehh@gmail.com>
2023-05-29 10:55:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.write('Hello World')
+
+# User input section
+st.title("Find Your Workout Buddy")
+st.write("Enter your exercise preferences:")
+
+gender = st.selectbox("Gender", ["Male", "Female", "Other"])
+sport = st.text_input("Type of Sport")
